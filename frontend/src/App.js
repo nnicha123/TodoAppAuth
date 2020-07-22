012fcee2bb7620ac8708ea3a091d53df39b3cdc1
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-22 09:53:25 +0700

    added react-routes to the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import TodoList from './TodoList'
 import axios from './config/axios'
 import Login from './Login'
+import Nav from './Nav'
+import { BrowserRouter as Router, NavLink, Redirect, BrowserRouter } from 'react-router-dom'
+import Route from 'react-router-dom/Route'
+import User from './User'
 
 export class App extends Component {
   state = {
-    todo : []
+    todo: []
   }
   componentDidMount = () => {
     // axios.get('http://localhost:8000/todos/').then(res => console.log(res.data))
@@ -13,8 +17,12 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <Login/>
-        {/* <TodoList/> */}
+        <Nav />
+        <Router className="router">
+          <Route exact path='/' component={Login} />
+          <Route exact path='/profile' component={User} />
+        </Router>
+
       </div>
     )
   }
--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Nav.css'
 
 function Nav() {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <li onClick={() => window.location.replace('/profile')}>Home</li>
+        </nav>
     )
 }
 
