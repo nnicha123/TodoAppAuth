a91e13292b0b564d4db801ba76ad36ee08296dd8
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-22 15:13:44 +0700

    todolist -> this project uses only react hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import TodoList from './TodoList'
 import Login from './Login'
 import Nav from './Nav'
-import { BrowserRouter as Router, NavLink, Redirect, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Route from 'react-router-dom/Route'
 import User from './User'
 import Register from './Register'
 
-export class App extends Component {
-  state = {
-    todo: []
-  }
-  componentDidMount = () => {
-    // axios.get('http://localhost:8000/todos/').then(res => console.log(res.data))
-  }
-  render() {
-    return (
-      <div>
-        <Nav />
-        <Router className="router">
-          <Route exact path='/' component={Login} />
-          <Route exact path='/profile' component={User} />
-          <Route exact path='/todo' component={TodoList}/>
-          <Route exact path='/register' component={Register}/>
-        </Router>
-
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Router className="router">
+        <Route exact path='/' component={Login} />
+        <Route exact path='/profile' component={User} />
+        <Route exact path='/todo' component={TodoList} />
+        <Route exact path='/register' component={Register} />
+      </Router>
+    </div>
+  )
 }
 
 export default App
